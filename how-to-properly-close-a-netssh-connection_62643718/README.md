7b90d94b2ae6d4ac1b07cbd1bbd94cb6bfc605f2
Kevin Gilpin <kgilpin@gmail.com>
2020-06-30 14:26:06 -0400

    Initial commit of how-to-properly-close-a-netssh-connection_62643718

---
+++ how-to-properly-close-a-netssh-connection_62643718/README.md
@@ -0,0 +1,12 @@
+# How to properly close a Net::SSH connection?
+
+Demonstration program for Stack Overflow question https://stackoverflow.com/questions/62643718/how-to-properly-close-a-netssh-connection.
+
+# Usage
+
+```
+docker-compose build
+docker-compose up -d
+ruby main.rb
+appland upload 
+```
---
+++ how-to-properly-close-a-netssh-connection_62643718/main.rb
@@ -0,0 +1,41 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require 'fileutils'
+require 'appmap'
+require 'net/ssh'
+require 'pry'
+
+port = `docker-compose port ssh-server 22`.strip.split(':').last
+puts "Connecting to localhost on #{port}"
+
+# binding.pry
+
+demo = lambda do
+  Net::SSH.start('localhost', 'test', port: port) do |ssh|
+    ssh.exec! 'touch /tmp/file'
+    puts "Hostname is: #{ssh.exec!('hostname')}"
+
+    ssh.open_channel do |ch|
+      ch.exec "sudo -p 'sudo password: ' ls" do |ch, success|
+        abort 'could not execute sudo ls' unless success
+
+        ch.on_data do |ch, data|
+          print data
+          if data =~ /sudo password: /
+            ch.send_data("password\n")
+          end
+        end
+      end
+    end
+
+    ssh.loop
+  end
+end
+
+FileUtils.mkdir_p 'tmp'
+appmap = AppMap.record do
+  demo.call
+end
+
+File.write('appmap.yml', JSON.generate(appmap))
